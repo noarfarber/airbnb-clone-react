fd1492685d4c6fd472eb02cdd1203386361e6fc3
noarfarber <noaros88@gmail.com>
2022-05-18 17:42:01 +0100

    added Navbar component with logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import logo from '../images/app-logo.png'
+
+const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			<img src={logo} className="App-logo" alt="logo" />
+		</nav>
+	)
+}
+
+export default Navbar;
